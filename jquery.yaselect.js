3bfcbdf4772dfbb991c25eff172a2d6a2b84a204
schourode <jsr@malamute.dk>
2011-07-18 11:41:19 +0200

    Fixes IE7 bug where toggling of yaselect-open/close classes could go "out-of-sync", removing yaselect-close class to favor more simple styling.

--- jquery.yaselect.js
+++ jquery.yaselect.js
@@ -17,12 +17,12 @@ jQuery.fn.yaselect = function(options) {
   return this.each(function(index, select) {
     var jselect = jQuery(select).css({position:'absolute',top:0,left:0,zIndex:1}).addClass('yaselect-select'),
         wrap    = jQuery('<div class="yaselect-wrap" tabindex="0"><div class="yaselect-current"></div></div>'),
-        anchor  = wrap.wrap('<div class="yaselect-anchor yaselect-open" style="position:relative;"></div>').parent(),
+        anchor  = wrap.wrap('<div class="yaselect-anchor" style="position:relative;"></div>').parent(),
+        toggle  = function() { anchor.toggleClass('yaselect-open', jselect.is(':visible')); },
         gettext = function() { return jselect.find('option:nth(' + (select.selectedIndex || 0) + ')').text(); },
         curr    = wrap.find('.yaselect-current').text(gettext()),
         confirm = function(to_focus_wrap) { jselect.blur(); curr.text(gettext()); jselect.css({top: wrap.outerHeight()}); if (to_focus_wrap) wrap.focus(); };
     if (options.hoverOnly || window.navigator && navigator.userAgent.match(/iphone|ipod|ipad/i)) {
-      anchor.toggleClass('yaselect-open yaselect-close');
       return jselect /* becomes invisible and is placed above wrapper to receive screen tap -- triggering native <select> */
         .before(anchor)
         .css({opacity: 0.001})
@@ -33,11 +33,11 @@ jQuery.fn.yaselect = function(options) {
       .before(anchor)
       .keydown(function(e) { if (e.which==13||e.which==32) { e.preventDefault(); confirm(true); } })
       .change(function(e) { curr.text(gettext()); })
-      .blur(function(e) { jselect.hide(); anchor.toggleClass('yaselect-open yaselect-close'); })
+      .blur(function(e) { jselect.hide(); toggle(); })
       .click(function(e) {
         if (jselect.is(':hidden')) {
-          anchor.toggleClass('yaselect-open yaselect-close');
           jselect.show();
+          toggle();
           setTimeout(function() { jselect.focus(); }); /* avoid trampling confusion with triggered blur */
         } else {
           confirm(true);
